1d6a94be8f7962af1bcd0c5a5f60fc6442c51701
axeliono <69375446+axeliono@users.noreply.github.com>
2021-02-15 13:38:42 -0600

    work done

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thought-controller");
+
+// /api/thoughts
+router.route("/").get(getAllThoughts);
+
+// /api/thoughts/<thoughtId>
+router.route("/:thoughtId").get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/<userId>
+router.route("/:userId").post(createThought);
+
+// /api/thoughts/<thoughtId>/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/<thoughtId>/reactions/<reactionId>
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
